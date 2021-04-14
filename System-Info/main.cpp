2c463320b3e4a3460d7ab843be9fcce93a4cbc97
Jackson <thepotato456@yahoo.com>
2021-04-14 11:36:05 -0500

    Initial Commit

---
+++ System-Info/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  System-Info
+//
+//  Created by Jackson Fortner on 4/14/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
